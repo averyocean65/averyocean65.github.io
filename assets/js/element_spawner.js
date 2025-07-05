33c38b97b96556a3d59bb4c208c7a2a69de79338
averyocean65 <averytoad65@gmail.com>
2025-07-05 12:13:05 +0200

    work on website

---
+++ assets/js/element_spawner.js
@@ -0,0 +1,36 @@
+fillers = [ "topbar", "ad-marquee" ]
+
+window.onload = (event) => {
+    for (let i = 0; i < fillers.length; i++) {
+        const name = fillers[i];
+        const path = `/assets/html/${name}.html`
+        const filler = document.getElementById(`${name}-filler`)
+
+        if(filler === null) continue;
+        fetch(path)
+            .then((response) => {
+                if(!response.ok) return "";
+                return response.text();
+            })
+            .then((text) => {
+                filler.innerHTML = text;
+
+                const inserter = document.getElementById("to-head")
+                if(inserter === null) return;
+                const scripts = inserter.querySelectorAll("script")
+
+                scripts.forEach(orig => {
+                    const newScript = document.createElement("script")
+                    for(const attr of orig.attributes)
+                        newScript.setAttribute(attr.name, attr.value)
+
+                    if(orig.textContent)
+                        newScript.textContent = orig.textContent
+
+                    document.head.appendChild(newScript)
+                })
+
+                inserter.remove();
+            })
+    }
+}
