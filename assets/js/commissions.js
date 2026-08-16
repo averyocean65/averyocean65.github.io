23c26364e8ed2a1e37f54a6db786696d08e7e05b
averyocean65 <averytoad65@gmail.com>
2026-08-16 19:38:05 +0200

    finish price calculator

--- assets/js/commissions.js
+++ assets/js/commissions.js
@@ -1,8 +1,47 @@
+var latest_price = 123
+
 function getPricePerMinute() {
-    return 60; // TODO: use select option to determine price
+    const selector = document.getElementById("genre");
+
+    console.log(selector.value)
+    switch (selector.value) {
+        case "cinematic":
+            return 80;
+        case "rock":
+            return 60;
+        case "electronic":
+            return 50;
+        case "mixing":
+            return 45;
+        default:
+            alert("Invalid genre/music type specified.")
+            break;
+    }
 }
 
 function calculatePrice() {
     const output = document.getElementById("calculated-price");
-    output.textContent = "Price: TODO (" + getPricePerMinute() + "€/min)";
+
+    const duration = document.getElementById("duration");
+    const purchase_button = document.getElementById("purchase-button");
+
+    if(duration.value === "") {
+        alert("Please input a valid duration!");
+        return;
+    }
+
+    const price_unit = getPricePerMinute();
+
+    const minutes = parseInt(duration.value.substring(0, 2));
+    const seconds = parseInt(duration.value.substring(3));
+
+    const totalInMinutes = minutes + (seconds / 60);
+    latest_price = Math.round((totalInMinutes * price_unit) * 100) / 100;
+
+    output.textContent = "Price: " + latest_price + "€ (" + price_unit + "€/min, " + duration.value + ")";
+    purchase_button.hidden = false;
+}
+
+function payOnThomann() {
+    openLinkCautious("https://www.thomann.de/intl/basket_coupon.html?ordervalue=" + latest_price)
 }
